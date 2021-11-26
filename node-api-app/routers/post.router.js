65ad92909f99e19595e2d15affceebe004142219
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2021-11-26 14:35:46 +0200

    implemented reading & writing to DB file

--- node-api-app/routers/post.router.js
+++ node-api-app/routers/post.router.js
@@ -1,13 +1,43 @@
 const express = require("express");
+const fs = require("fs/promises");
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+
+    //Reading hardcoded posts DB
+
+    let parsedData = {};
+
+    try {
+        const rawData = await fs.readFile("./posts.db.json", "utf-8");
+        parsedData = JSON.parse(rawData);
+        console.log("Reading posts.db.json file...");
+    } catch (error) {
+        console.error('There was an error:', error.message);
+    }
+
     res.json({
+        posts: parsedData.posts,
+        count: parsedData.posts.length
     })
 });
 
 router.post('/', async (req, res) => {
+
+    try {
+        //Reading hardcoded posts DB
+        const rawData = await fs.readFile("./posts.db.json", "utf-8");
+        const data = JSON.parse(rawData);
+        console.log("Reading posts.db.json file...");
+        data.posts.push(req.body);
+        //Writing to hardcoded posts DB
+        fs.writeFile("./posts.db.json", JSON.stringify(data));
+        console.log("Adding new post to posts.db.json file...");
+    } catch (error) {
+        console.error('There was an error:', error.message);
+    }
+
     res.sendStatus(201);
 });
 
