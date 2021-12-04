e2a58378f093e708660221decfcbda83a9aadfa3
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2021-12-04 19:19:20 +0200

    added routing for pages & modal

--- react-blog-app/src/App.tsx
+++ react-blog-app/src/App.tsx
@@ -1,34 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Modal from "./components/Modal";
 import MainPage from "./pages/MainPage";
-import NewArticleForm from "./pages/NewArticleForm";
-import FormSuccess from "./pages/FormSuccess";
+import ArticlePage from "./pages/ArticlePage";
+import NewArticlePage from "./pages/NewArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import NewArticleModal from "./modals/NewArticleModal";
 
 const App = () => {
 
-    const [isNewArticleOpen, setIsNewArticleOpen] = useState(false);
-    const [isNewArticleSubmitted, setIsNewArticleSubmitted] = useState(false);
+    const location = useLocation<any>();
+    const background = location.state && location.state.background;
 
     return (
         <div className="app">
-            {
-                isNewArticleOpen && !isNewArticleSubmitted &&
-                <NewArticleForm
-                    setOpenClosed={setIsNewArticleOpen}
-                    setIsSubmitted={setIsNewArticleSubmitted}
-                />
-            }
-            {
-                isNewArticleSubmitted && !isNewArticleOpen &&
-                <FormSuccess
-                    setOpenClosed={setIsNewArticleSubmitted}
-                />
-            }
-            <Header  setIsNewArticleOpen={setIsNewArticleOpen} />
-            <MainPage isNewArticleSubmitted={isNewArticleSubmitted} />
+            <Header />
+
+            {/* Routing */}
+            <Switch location={ background || location }>
+                <Route exact path="/" component={MainPage} />
+                <Route exact path="/newarticle" component={NewArticlePage} />
+                <Route path="/article_:articleId" component={ArticlePage} />
+                <Route path="*" component={NotFoundPage} />
+            </Switch>
+
+            {background && background.pathname !== "/newarticle" && <Route exact path="/newarticle" children={
+                <Modal>
+                    <NewArticleModal />
+                </Modal>
+            } />}
+
             <Footer />
         </div>
     );
