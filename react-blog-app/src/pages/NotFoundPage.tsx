4cff6b941f706ae5a55df4867a9e68c11ae5abce
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2021-12-04 17:56:13 +0200

    added 404 page

---
+++ react-blog-app/src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return(
+        <main className="main-container">
+            <p>Error 404: Not Found</p>
+        </main>
+    )
+}
+
+export default NotFoundPage;
