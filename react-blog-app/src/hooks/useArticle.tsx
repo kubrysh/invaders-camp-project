260441f9fa0467816cce086169d55b0ea820cc79
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2021-12-05 20:04:00 +0200

    preventing state changing on unmounted components

--- react-blog-app/src/hooks/useArticle.tsx
+++ react-blog-app/src/hooks/useArticle.tsx
@@ -1,23 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-
-const baseURL = `${process.env.REACT_APP_API_URL}/api/posts`;
 
 const useArticle = (id:any) => {
 
     const baseURL = `${process.env.REACT_APP_API_URL}/api/posts/${id}`;
 
+    const isCurrent = useRef(true);
     const [article, setArticle] = useState({});
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        return () => {
+            isCurrent.current = false;
+        };
+    }, []);
 
     useEffect(() => {
         const fetchArticle = async () => {
             setIsLoading(true);
 
             try {
-                const { data } = await axios.get(baseURL);    
+                const { data } = await axios.get(baseURL);
+                //Preventing changing state of an unmounted component
+                if (isCurrent.current) {
                     setArticle(data);
-                setIsLoading(false);
+                    setIsLoading(false);
+                }
             } catch (e:any) {
                 setError(e);
                 console.error(e);
--- react-blog-app/src/hooks/useArticles.tsx
+++ react-blog-app/src/hooks/useArticles.tsx
@@ -1,28 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-
-const baseURL = `${process.env.REACT_APP_API_URL}/api/posts`;
 
 const useArticles = () => {
 
+    const baseURL = `${process.env.REACT_APP_API_URL}/api/posts`;
+
+    const isCurrent = useRef(true);
     const [articles, setArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        return () => {
+            isCurrent.current = false;
+        };
+    }, []);
 
     useEffect(() => {
         const fetchArticles = async () => {
             setIsLoading(true);
 
             try {
-                const { data } = await axios.get(baseURL);    
-                setArticles(data.posts);
-                setIsLoading(false);
+                const { data } = await axios.get(baseURL);
+                //Preventing changing state of an unmounted component
+                if (isCurrent.current) {
+                    setArticles(data.posts);
+                    setIsLoading(false);
+                }
             } catch (e) {
                 console.error(e);
             }
         };
 
         fetchArticles();
-    }, []);
+    }, [baseURL]);
 
     return { articles, isLoading };
 }
